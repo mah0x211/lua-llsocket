9066a825f2f57b1911c696ce53cbbdde45b09c06
Masatoshi Teruya <mah0x211@gmail.com>
2016-01-15 22:36:37 +0900

    change: test/listen_try.lua has been updated.

--- test/listen_try.lua
+++ test/listen_try.lua
@@ -41,7 +41,7 @@ local function test( addr, file )
     -- create socket
     sock = ifNil( socket.new( addr ) );
     ifNotTrue( sock:reuseaddr( true ) );
-    ifNotTrue( sock:bind() );
+    ifNotNil( sock:bind() );
     ifNotNil( sock:listen() );
     ifNotNil( sock:close() );
     if file then
@@ -50,7 +50,7 @@ local function test( addr, file )
     -- w nonblock
     sock = ifNil( socket.new( addr, NONBLOCK ) );
     ifNotTrue( sock:reuseaddr( true ) );
-    ifNotTrue( sock:bind() );
+    ifNotNil( sock:bind() );
     ifNotNil( sock:listen() );
     ifNotNil( sock:close() );
     if file then
