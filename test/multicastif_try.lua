abda16dc0b07b86894175dfdf1b384d0861798e8
Masatoshi Teruya <mah0x211@gmail.com>
2016-01-17 13:35:43 +0900

    add: test/multicastif_try.lua has been added.

---
+++ test/multicastif_try.lua
@@ -0,0 +1,55 @@
+local llsocket = require('llsocket');
+local SOCK_STREAM = llsocket.SOCK_STREAM;
+local SOCK_DGRAM = llsocket.SOCK_DGRAM;
+local socket = llsocket.socket;
+local getaddrinfoInet = llsocket.inet.getaddrinfo;
+local getaddrinfoUnix = llsocket.unix.getaddrinfo;
+local HOST = '127.0.0.1';
+local SOCKFILE = './test.sock';
+
+
+local function test_sup( addr )
+    -- create socket
+    local sock = ifNil( socket.new( addr ) );
+
+    ifNotEqual( sock:multicastif( '0.0.0.0' ), '0.0.0.0' );
+    ifNotEqual( sock:multicastif(), '0.0.0.0' );
+    ifNotEqual( sock:multicastif( '127.0.0.1' ), '127.0.0.1' );
+    ifNotEqual( sock:multicastif(), '127.0.0.1' );
+
+    ifNotNil( sock:close() );
+end
+
+
+local function test_nosup( addr )
+    -- create socket
+    local sock = ifNil( socket.new( addr ) );
+
+    ifNotNil( sock:multicastif() );
+    ifNotNil( sock:multicastif( '0.0.0.0' ) );
+
+    ifNotNil( sock:close() );
+end
+
+
+-- stream socket
+addrs = ifNil( getaddrinfoInet( HOST, PORT, SOCK_STREAM ) );
+for _, addr in ipairs( addrs ) do
+    test_nosup( addr );
+end
+-- via unix domain
+addrs = ifNil( getaddrinfoUnix( SOCKFILE, SOCK_STREAM ) );
+test_nosup( addrs );
+
+
+-- dgram socket
+addrs = ifNil( getaddrinfoInet( HOST, PORT, SOCK_DGRAM ) );
+for _, addr in ipairs( addrs ) do
+    test_sup( addr );
+end
+-- via unix domain
+addrs = ifNil( getaddrinfoUnix( SOCKFILE, SOCK_DGRAM ) );
+test_nosup( addrs );
+
+
+
