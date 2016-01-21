2d08c83c6b7e4cf659c8c41f4eb70306b3c29828
Masatoshi Teruya <mah0x211@gmail.com>
2016-01-21 19:10:59 +0900

    change: test/membership_try.lua has been updated.

--- test/membership_try.lua
+++ test/membership_try.lua
@@ -5,17 +5,17 @@ local socket = llsocket.socket;
 local getaddrinfoInet = llsocket.inet.getaddrinfo;
 local getaddrinfoUnix = llsocket.unix.getaddrinfo;
 local HOST = '127.0.0.1';
+local PORT = '5000';
 local SOCKFILE = './test.sock';
-
 
 local function test_sup( addr )
     -- create socket
     local sock = ifNil( socket.new( addr ) );
 
-    ifNotNil( sock:addmembership( '225.1.1.1' ) );
-    ifNotNil( sock:dropmembership( '225.1.1.1' ) );
-    ifNotNil( sock:addmembership( '225.1.1.1', '0.0.0.0' ) );
-    ifNotNil( sock:dropmembership( '225.1.1.1', '0.0.0.0' ) );
+    ifNotNil( sock:mcastjoin( '224.0.0.251' ) );
+    ifNotNil( sock:mcastleave( '224.0.0.251' ) );
+    ifNotNil( sock:mcastjoin( '224.0.0.251', 'lo0' ) );
+    ifNotNil( sock:mcastleave( '224.0.0.251', 'lo0' ) );
 
     ifNotNil( sock:close() );
 end
@@ -25,12 +25,11 @@ local function test_nosup( addr )
     -- create socket
     local sock = ifNil( socket.new( addr ) );
 
-    ifNil( sock:addmembership( '225.1.1.1' ) );
-    ifNil( sock:dropmembership( '225.1.1.1' ) );
+    ifNil( sock:mcastjoin( '224.0.0.251' ) );
+    ifNil( sock:mcastleave( '224.0.0.251' ) );
 
     ifNotNil( sock:close() );
 end
-
 
 -- stream socket
 addrs = ifNil( getaddrinfoInet( HOST, PORT, SOCK_STREAM ) );
@@ -40,6 +39,7 @@ end
 -- via unix domain
 addrs = ifNil( getaddrinfoUnix( SOCKFILE, SOCK_STREAM ) );
 test_nosup( addrs );
+os.remove( SOCKFILE );
 
 
 -- dgram socket
@@ -50,6 +50,6 @@ end
 -- via unix domain
 addrs = ifNil( getaddrinfoUnix( SOCKFILE, SOCK_DGRAM ) );
 test_nosup( addrs );
-
+os.remove( SOCKFILE );
 
 
