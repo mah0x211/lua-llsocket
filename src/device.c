95b41db3063ec49904cd443cf8252a3b956dc897
Masatoshi Fukunaga <mah0x211@gmail.com>
2021-10-28 01:08:21 +0900

    Remove device.macadddrs function

--- src/device.c
+++ src/device.c
@@ -30,110 +30,6 @@
 #include <ifaddrs.h>
 #include <sys/ioctl.h>
 
-// MARK: device info
-#if defined(__linux__)
-
-static int macaddrs_lua(lua_State *L)
-{
-    int fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-    if (fd) {
-        char buf[INET6_ADDRSTRLEN];
-        struct ifreq ifrbuf[16];
-        struct ifconf ifc = {.ifc_len = sizeof(ifrbuf), .ifc_req = ifrbuf};
-
-        // get if-conf
-        if (ioctl(fd, SIOCGIFCONF, &ifc) != -1) {
-            struct ifreq *it   = ifc.ifc_req;
-            struct ifreq *end  = it + (ifc.ifc_len / sizeof(struct ifreq));
-            unsigned char *mac = NULL;
-
-            lua_newtable(L);
-            for (; it != end; it++) {
-                // inet only
-                if (it->ifr_addr.sa_family == AF_INET &&
-                    // running only and ignore loopback
-                    ioctl(fd, SIOCGIFFLAGS, it) != -1 &&
-                    it->ifr_flags & IFF_RUNNING &&
-                    !(it->ifr_flags & IFF_LOOPBACK) &&
-                    // get flags and hardware address
-                    ioctl(fd, SIOCGIFHWADDR, it) != -1) {
-                    mac = (unsigned char *)it->ifr_hwaddr.sa_data;
-
-                    snprintf(buf, INET6_ADDRSTRLEN,
-                             "%02x:%02x:%02x:%02x:%02x:%02x", *mac, mac[1],
-                             mac[2], mac[3], mac[4], mac[5]);
-                    lauxh_pushstr2tbl(L, it->ifr_name, buf);
-                }
-            }
-
-            close(fd);
-            return 1;
-        }
-
-        close(fd);
-    }
-
-    // got error
-    lua_pushnil(L);
-    lua_pushstring(L, strerror(errno));
-
-    return 2;
-}
-
-#else
-
-static int macaddrs_lua(lua_State *L)
-{
-    struct ifaddrs *ifa;
-
-    if (getifaddrs(&ifa) == 0) {
-        lua_newtable(L);
-
-        if (ifa) {
-            struct ifaddrs *ptr = ifa;
-            char buf[INET6_ADDRSTRLEN];
-            unsigned char *mac = NULL;
-            struct sockaddr_dl *sd;
-
-            do {
-                if (ptr->ifa_addr->sa_family == AF_LINK) {
-                    sd = (struct sockaddr_dl *)ptr->ifa_addr;
-                    switch (sd->sdl_alen) {
-                    case 6:
-                        mac = (unsigned char *)LLADDR(sd);
-                        snprintf(buf, INET6_ADDRSTRLEN,
-                                 "%02x:%02x:%02x:%02x:%02x:%02x", *mac, mac[1],
-                                 mac[2], mac[3], mac[4], mac[5]);
-                        lauxh_pushstr2tbl(L, ptr->ifa_name, buf);
-                        break;
-                    case 8:
-                        mac = (unsigned char *)LLADDR(sd);
-                        snprintf(buf, INET6_ADDRSTRLEN,
-                                 "%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x",
-                                 *mac, mac[1], mac[2], mac[3], mac[4], mac[5],
-                                 mac[6], mac[7]);
-                        lauxh_pushstr2tbl(L, ptr->ifa_name, buf);
-                        break;
-                    }
-                }
-
-                ptr = ptr->ifa_next;
-            } while (ptr);
-        }
-
-        freeifaddrs(ifa);
-        return 1;
-    }
-
-    // got error
-    lua_pushnil(L);
-    lua_pushstring(L, strerror(errno));
-
-    return 2;
-}
-#endif
-
 static inline int gettable(lua_State *L, int tblidx, const char *name)
 {
     lua_pushstring(L, name);
@@ -372,7 +268,6 @@ static int getifaddrs_lua(lua_State *L)
 LUALIB_API int luaopen_llsocket_device(lua_State *L)
 {
     struct luaL_Reg method[] = {
-        {"macaddrs",   macaddrs_lua  },
         {"getifaddrs", getifaddrs_lua},
         {NULL,         NULL          }
     };
